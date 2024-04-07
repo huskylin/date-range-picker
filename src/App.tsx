c9a4967241a8140f18efb4ab1b38a0ace2dca99a
huskylin <hank84112092@google.com>
2024-04-07 14:01:08 +0800

    chore: init react webpack babel

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Calendar Demo Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['.*', '.js', '.jsx', '.tsx', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
